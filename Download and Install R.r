8cbae1658297df5975859c57475ce99ba33e5e64
Prashant Kandpal <126224800+kd-prashant@users.noreply.github.com>
2024-11-09 07:46:31 +0530

    Download and Install R
    R programming is a language specifically designed for data analysis, statistics, and data visualization, making it a popular choice in fields like AI, ML, and data science.

---
+++ Download and Install R.r
@@ -0,0 +1,13 @@
+# Installing basic packages
+install.packages("ggplot2")    # Data visualization
+install.packages("dplyr")      # Data manipulation
+install.packages("tidyr")      # Data cleaning
+install.packages("readr")      # Reading data
+install.packages("caret")      # Machine learning
+
+# Load packages after installation
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(caret)
